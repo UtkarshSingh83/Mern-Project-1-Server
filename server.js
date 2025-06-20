389e0e2b6cc83ece1386909e725f6d8eda5503ce
UtkarshSingh83 <121507514+UtkarshSingh83@users.noreply.github.com>
2025-06-20 16:17:08 +0530

    Cors integration

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');   // Include the express module
-const cookieParser = require('cookie-parser'); 
-const authRoutes = require('./src/routes/authRoutes'); 
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./src/routes/authRoutes');
+const cors = require('cors');
 
 const app = express();     // Instantiate express app (creating object)
 
 app.use(express.json()); // Middleware to convert json to javascript objects
 app.use(cookieParser());
+
+const corsOption = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+};
+
+app.use(cors(corsOption));
 app.use('/auth', authRoutes);
 
 const PORT = 5001;
